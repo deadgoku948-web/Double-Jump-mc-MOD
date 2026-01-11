71749ac653d2bc29967debaa207e361dd8f73a23
deadgoku948-web <deadgoku948@gmail.com>
2026-01-10 19:12:37 -0500

    Add files via upload

---
+++ deepseek_java_20260111_90df94.java
@@ -0,0 +1,29 @@
+package com.example.doublejump;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = DoubleJumpMod.MODID, name = DoubleJumpMod.NAME, version = DoubleJumpMod.VERSION)
+public class DoubleJumpMod {
+    public static final String MODID = "doublejumpmod";
+    public static final String NAME = "Double Jump Mod";
+    public static final String VERSION = "1.0";
+    
+    private static Logger logger;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+        MinecraftForge.EVENT_BUS.register(new DoubleJumpHandler());
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        logger.info("Double Jump Mod initialized!");
+    }
+}
