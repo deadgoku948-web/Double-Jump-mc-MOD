d628978675bb3457ef78209c5c59dea36393a398
deadgoku948-web <deadgoku948@gmail.com>
2026-01-10 19:36:09 -0500

    Add files via upload

---
+++ deepseek_java_20260111_335f3d.java
@@ -0,0 +1,26 @@
+@Mod(modid = DoubleJumpMod.MODID, name = DoubleJumpMod.NAME, version = DoubleJumpMod.VERSION,
+     acceptedMinecraftVersions = "[1.8.8]", useMetadata = true)
+public class DoubleJumpMod {
+    public static final String MODID = "doublejumpmod";
+    public static final String NAME = "Double Jump Mod";
+    public static final String VERSION = "1.0";
+    
+    @SidedProxy(clientSide = "com.example.doublejump.ClientProxy", 
+                serverSide = "com.example.doublejump.CommonProxy")
+    public static CommonProxy proxy;
+    
+    private static Logger logger;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+        proxy.preInit(event);
+        MinecraftForge.EVENT_BUS.register(new DoubleJumpHandler());
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+        logger.info("Double Jump Mod initialized!");
+    }
+}
