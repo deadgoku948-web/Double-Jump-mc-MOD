2349bb15a9325804c476db6a61d0319e15d43154
deadgoku948-web <deadgoku948@gmail.com>
2026-01-10 19:33:14 -0500

    Add files via upload

---
+++ deepseek_java_20260111_80741d.java
@@ -0,0 +1,30 @@
+package com.example.doublejump;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.input.Keyboard;
+
+@SideOnly(Side.CLIENT)
+public class ClientProxy extends CommonProxy {
+    
+    public static KeyBinding rechargeKey;
+    
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+    }
+    
+    @Override
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+        
+        // Register key bindings
+        rechargeKey = new KeyBinding("Recharge Stamina", Keyboard.KEY_R, "Double Jump Mod");
+        ClientRegistry.registerKeyBinding(rechargeKey);
+    }
+}
